edb6811c1d7720f32f4f0a8a0b25f4a39a313c42
lacki28 <anna@lackinger.net>
2023-04-11 15:28:52 +0200

    fix bugs in sort files by tasks

--- data-description-plots/sort_files_by_nr_of_tasks.py
+++ data-description-plots/sort_files_by_nr_of_tasks.py
@@ -9,21 +9,21 @@ def read_data(path=''):
     for file in filepaths:
         csv = pd.read_csv(file, sep=",")
         if csv["nr_of_tasks"][1] == 2:
-            df_list.append(csv)
+            df_list.append(file[24:])
     return df_list
 
 def add_files_into_dir(path,filepaths):
     for file in filepaths:
-        df= pd.read_csv(file, sep=",")
+        df= pd.read_csv("./sortedGroupedJobFiles/"+file, sep=",")
         df.to_csv(path+file, index=False)
 
 def main():
-    training_data_files = read_data("/sortedGroupedJobFiles")
+    training_data_files = read_data("./sortedGroupedJobFiles")
     test_head = int(0.8 * len(training_data_files))
     df_train = training_data_files[:test_head]
     df_test = training_data_files[test_head:len(training_data_files)]
-    add_files_into_dir("../training/",df_train)
-    add_files_into_dir("../test/", df_test)
+    add_files_into_dir("./training/",df_train)
+    add_files_into_dir("./test/", df_test)
 
 
 if __name__ == "__main__":
