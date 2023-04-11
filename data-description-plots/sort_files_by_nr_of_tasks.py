7caf6a5940d353adcd7fa4c13d8aa35d8de42ede
lacki28 <anna@lackinger.net>
2023-04-11 15:16:28 +0200

    change sort files, to add files to directory

--- data-description-plots/sort_files_by_nr_of_tasks.py
+++ data-description-plots/sort_files_by_nr_of_tasks.py
@@ -1,12 +1,9 @@
 import os
 
-import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from scipy.signal import savgol_filter
 
 
-def read_data(path='../training'):
+def read_data(path=''):
     filepaths = [path + "/" + f for f in os.listdir(path) if f.endswith('.csv')]
     df_list = list()
     for file in filepaths:
@@ -15,11 +12,18 @@ def read_data(path='../training'):
             df_list.append(csv)
     return df_list
 
+def add_files_into_dir(path,filepaths):
+    for file in filepaths:
+        df= pd.read_csv(file, sep=",")
+        df.to_csv(path+file, index=False)
 
 def main():
-    training_data_files = read_data("../training")
-    for data in training_data_files:
-        print(data[data["nr_of_tasks"]!=2])
+    training_data_files = read_data("/sortedGroupedJobFiles")
+    test_head = int(0.8 * len(training_data_files))
+    df_train = training_data_files[:test_head]
+    df_test = training_data_files[test_head:len(training_data_files)]
+    add_files_into_dir("../training/",df_train)
+    add_files_into_dir("../test/", df_test)
 
 
 if __name__ == "__main__":
