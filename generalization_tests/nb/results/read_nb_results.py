7fe3a5bb7f31348c7beb0e66b2c2283543371f30
lacki28 <anna@lackinger.net>
2023-08-02 15:20:49 +0200

    refactor nb results

--- generalization_tests/nb/results/read_nb_results.py
+++ generalization_tests/nb/results/read_nb_results.py
@@ -1,3 +1,5 @@
+import statistics
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -12,8 +14,8 @@ def remove_outliers(data):
 
         IQR = Q3 - Q1
 
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
+        lower_bound = Q1 - 2 * IQR
+        upper_bound = Q3 + 2 * IQR
         print(len(data_arr))
         print(len([x for x in data_arr if lower_bound <= x <= upper_bound]))
         cleaned_data.append([x for x in data_arr if lower_bound <= x <= upper_bound])
@@ -22,30 +24,26 @@ def remove_outliers(data):
 
 
 def create_boxplot(loss, name, timestamp, dir):
+    print(dir + " " + name)
     loss = remove_outliers(loss)
-    print(loss)
-    print(name)
-    plt.boxplot(loss, labels=[name])
-    plt.xlabel('Loss value')
-    plt.ylabel('Metric')
-    plt.title('Naive benchmark loss values for ' + str(timestamp + 1) + ' ahead prediction')
-    plt.savefig('NB_' + dir + '_' + name + '_loss_t' + str(timestamp + 1) + '.png')
-
-def create_boxplot(loss, name, timestamp, dir):
-    loss = remove_outliers(loss)
-    data = [loss[0], loss[1], loss[2], loss[3], loss[4], loss[5]]
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 8))
 
     plt.boxplot(loss)
-    plt.xlabel('Timestamps ahead', fontsize=18)
-    plt.ylabel('Metric ' + name, fontsize=18)
-    plt.title('Naive benchmark ' + name, fontsize=20)
-    plt.legend(["Naive benchmark"])
+    plt.xlabel('Timestamps ahead', fontsize=20)
+    plt.ylabel('Metric ' + name, fontsize=20)
+    plt.title('Naive benchmark ' + name, fontsize=22)
     plt.savefig('NB_' + dir + '_' + name + '_loss_t' + str(timestamp + 1) + '.png')
     plt.close()
 
+
 def calc_avg(lst):
     result = sum(lst) / len(lst)
+    rounded_result = round(result, 5)
+    return str(rounded_result)
+
+
+def calc_std(lst):
+    result = statistics.stdev(lst)
     rounded_result = round(result, 5)
     return str(rounded_result)
 
@@ -64,28 +62,34 @@ def get_avg_loss(dir):
         total_time = []
 
         with open(dir + str(timestamp + 1) + ".txt", 'r') as file:
-            counter = 0
             for line in file:
-                if counter % 2 == 1:
-                    test_error_values = line.split(' & ')
-                    mae.append(float(test_error_values[0]))
-                    mse.append(float(test_error_values[1]))
-                    r2.append(float(test_error_values[2]))
-                    nr.append(float(test_error_values[3]))
-                    total_time.append(float(test_error_values[4]))
-                counter += 1
-        with open('avg_' + dir + str(timestamp + 1) + ".txt", 'w') as file:
-            file.write("mae & mse & r2 & nr  & total \n")
+                test_error_values = line.split(' & ')
+                mae.append(float(test_error_values[0]))
+                mse.append(float(test_error_values[1]))
+                r2.append(float(test_error_values[2]))
+                nr.append(float(test_error_values[3]))
+                total_time.append(float(test_error_values[4]))
+        with open('avg_' + str(timestamp + 1) + ".txt", 'a+') as file:
             file.write(
-                calc_avg(mae) + " & " + calc_avg(mse) + " & " + calc_avg(r2) + " &  $\infty$ & 0 & " + calc_avg(total_time))
+                "\multirow{2}{*}{NB} & \multirow{2}{*}{" + dir + "} & {avg} & \multirow{2}{*}{-} &\multirow{2}{*}{4}& " +
+                calc_avg(mae) + " & " + calc_avg(mse) + " & " + calc_avg(r2) + " & \multirow{2}{*}{$\infty$} & " +
+                "\multirow{2}{*}{-}" + " & " "\multirow{2}{*}{" + calc_avg(
+                    total_time) + "}\\\\\n")
+            file.write(
+                "&   & std & & &" + calc_std(mae) + " & " + calc_std(mse) + " & " + calc_std(r2) + " &  &  & \\\\\n")
+
+        # with open('avg_' + dir + str(timestamp + 1) + ".txt", 'w') as file:
+        #     file.write("mae & mse & r2 & nr  & total \n")
+        #     file.write(
+        #         calc_avg(mae) + " & " + calc_avg(mse) + " & " + calc_avg(r2) + " &  $\infty$ & 0 & " + calc_avg(total_time))
         list_of_mae.append(mae)
         list_of_mse.append(mse)
         list_of_r2.append(r2)
         list_of_nr.append(nr)
         list_of_total_time.append(total_time)
-    create_boxplot(list_of_mae, "mae", timestamp, dir)
-    create_boxplot(list_of_mse, "mse", timestamp, dir)
-    create_boxplot(list_of_r2, "r2", timestamp, dir)
+    create_boxplot(list_of_mae, "MAE", timestamp, dir)
+    create_boxplot(list_of_mse, "MSE", timestamp, dir)
+    create_boxplot(list_of_r2, "R2", timestamp, dir)
 
 
 def create_files(file_name):
@@ -109,10 +113,10 @@ def create_files(file_name):
 
 
 def main():
-    create_files("train")
-    create_files("validation")
-    create_files("test")
-    get_avg_loss("train")
+    # create_files("train")
+    # create_files("validation")
+    # create_files("test")
+    # get_avg_loss("train")
     get_avg_loss("validation")
     get_avg_loss("test")
 
